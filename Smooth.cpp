7839f42f3fa01e81c4c07431aff87d067941e0aa
Karen W <karenweidev@gmail.com>
2021-12-08 22:26:21 +0800

    Reformat

--- Smooth.cpp
+++ Smooth.cpp
@@ -12,54 +12,63 @@ using namespace std;
 #define NSmooth 1000
 #define ThreadNUM 6
 
+// Initialize variables used for mutex barriers
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 int thread_count = 0;
 int thread_count_ = 0;
+
+// Variables for race condition
 bool isDone = false;
 bool isDone_ = false;
 
+// BMP variables
 BMPHEADER bmpHeader;            
 BMPINFO bmpInfo;
 RGBTRIPLE **BMPSaveData = NULL;                              
 RGBTRIPLE **BMPData = NULL;                                                   
 
-int readBMP(char *fileName); //read file
-int saveBMP(char *fileName); //save file
+int readBMP(char *fileName); // Reads BMP file
+int saveBMP(char *fileName); // Saves BMP file
 void swap(RGBTRIPLE *a, RGBTRIPLE *b);
-RGBTRIPLE **alloc_memory(int Y, int X); //allocate memory
-void *smooth(void *arg);
+RGBTRIPLE **alloc_memory(int Y, int X); // Allocate memory
+void *smooth(void *arg); // Image Smoothing
 void process_column(RGBTRIPLE **BMPData, BMPINFO bmpInfo, RGBTRIPLE **BMPTemp, int id);
 void process_row(int i, RGBTRIPLE **BMPData, BMPINFO bmpInfo, RGBTRIPLE **BMPTemp);
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
     char *infileName = (char*)"input.bmp";
     char *outfileName = (char*)"output.bmp";
 	
-    if ( readBMP( infileName) )
+	// Read BMP file
+    if(readBMP(infileName)) {
     	cout << "Read file successfully!!" << endl;
-    else 
+	} else { 
         cout << "Read file fails!!" << endl;
+	}
 
     BMPData = alloc_memory(bmpInfo.biHeight, bmpInfo.biWidth);
 
-	// threads
+	// Smoothing operations using threads
 	pthread_t tid[ThreadNUM];
-	unsigned int ints[ThreadNUM];
+	unsigned int thread_ids[ThreadNUM];
 
-	// smoothing operation using threads
 	for(int i = 0; i < ThreadNUM; i++) {
-		ints[i] = i;
-		pthread_create(&tid[i], NULL, smooth, &ints[i]);
+		thread_ids[i] = i;
+		pthread_create(&tid[i], NULL, smooth, &thread_ids[i]);
 	}
+
+	// Waiting for threads...
 	for(int i = 0; i < ThreadNUM; i++) {
 		pthread_join(tid[i], NULL);
 	 }
 
-    if ( saveBMP( outfileName ) )
+	// Save BMP file
+    if(saveBMP(outfileName)) {
     	cout << "Save file successfully!!" << endl;
-    else
+	} else {
         cout << "Save file fails!!" << endl;
+	}
 
 	pthread_mutex_destroy(&mutex);
 	free(BMPData);
@@ -69,31 +78,32 @@ int main(int argc,char *argv[])
 
 int readBMP(char *fileName)
 {
-    ifstream bmpFile( fileName, ios::in | ios::binary );
+    ifstream bmpFile(fileName, ios::in | ios::binary);
 
-    if (!bmpFile ){
+    if(!bmpFile) {
     	cout << "It can't open file!!" << endl;
        	return 0;
    	}
  
 	bmpFile.read((char*)&bmpHeader, sizeof(BMPHEADER));
  
-    if(bmpHeader.bfType != 0x4d42){
+    if(bmpHeader.bfType != 0x4d42) {
     	cout << "This file is not .BMP!!" << endl ;
         return 0;
     }
  
     bmpFile.read((char*)&bmpInfo, sizeof(BMPINFO));
         
-    if (bmpInfo.biBitCount != 24){
+    if(bmpInfo.biBitCount != 24) {
    		cout << "The file is not 24 bits!!" << endl;
         return 0;
    	}
 
-    while(bmpInfo.biWidth % 4 != 0)
+    while(bmpInfo.biWidth % 4 != 0) {
     	bmpInfo.biWidth++;
+	}
 
-    BMPSaveData = alloc_memory( bmpInfo.biHeight, bmpInfo.biWidth);
+    BMPSaveData = alloc_memory(bmpInfo.biHeight, bmpInfo.biWidth);
 	bmpFile.read((char*)BMPSaveData[0], bmpInfo.biWidth*sizeof(RGBTRIPLE)*bmpInfo.biHeight);
 	
     bmpFile.close();
@@ -101,40 +111,40 @@ int readBMP(char *fileName)
    	return 1;
 }
 
-int saveBMP( char *fileName)
+int saveBMP(char *fileName)
 {
 	if(bmpHeader.bfType != 0x4d42) {
     	cout << "This file is not .BMP!!" << endl ;
         return 0;
     }
         
-    ofstream newFile( fileName,  ios:: out | ios::binary );
+    ofstream newFile(fileName, ios::out | ios::binary);
  
-    if (!newFile){
+    if(!newFile) {
    		cout << "The File can't create!!" << endl;
-        return 0;  
+        return 0;
 	}
  	
-    newFile.write( ( char* )&bmpHeader, sizeof( BMPHEADER ) );
+    newFile.write((char*)&bmpHeader, sizeof(BMPHEADER));
 
-    newFile.write( ( char* )&bmpInfo, sizeof( BMPINFO ) );
+    newFile.write((char*)&bmpInfo, sizeof(BMPINFO));
 
-    newFile.write( ( char* )BMPSaveData[0], bmpInfo.biWidth*sizeof(RGBTRIPLE)*bmpInfo.biHeight );
+    newFile.write((char*)BMPSaveData[0], bmpInfo.biWidth*sizeof(RGBTRIPLE)*bmpInfo.biHeight);
 
     newFile.close();
  
     return 1;
 }
 
-RGBTRIPLE **alloc_memory(int Y, int X )
+RGBTRIPLE **alloc_memory(int Y, int X)
 {        
     RGBTRIPLE **temp = new RGBTRIPLE *[Y];
 	RGBTRIPLE *temp2 = new RGBTRIPLE [Y * X];
-   	memset( temp, 0, sizeof( RGBTRIPLE) * Y);
-   	memset( temp2, 0, sizeof( RGBTRIPLE ) * Y * X );
+   	memset(temp, 0, sizeof(RGBTRIPLE) * Y);
+   	memset(temp2, 0, sizeof(RGBTRIPLE) * Y * X );
 
-	for( int i = 0; i < Y; i++){
-        temp[ i ] = &temp2[i*X];
+	for(int i = 0; i < Y; i++) {
+        temp[i] = &temp2[i * X];
     }
  
     return temp; 
@@ -166,11 +176,12 @@ void *smooth(void *arg)
 			thread_count = 0;
 			isDone = true;
 		}
-
+		
+		// All threads wait for last thread to reset the variables
 		while(1)
 			if(isDone) break;
 
-		// updates BMPData (they do it in decreasing order)
+		// updates BMPData
 		pthread_mutex_lock(&mutex);
 		std::copy(&BMPTemp[0][0]+((total_area/ThreadNUM)*id), &BMPTemp[0][0]+((total_area/ThreadNUM)*(id+1)), &BMPData[0][0]+((total_area/ThreadNUM)*id));
 		pthread_mutex_unlock(&mutex);
@@ -232,6 +243,7 @@ void process_column(RGBTRIPLE **BMPData, BMPINFO bmpInfo, RGBTRIPLE **BMPTemp, i
 {
 	int start_i = ((bmpInfo.biHeight/ThreadNUM)*id);
 	int end_i = ((bmpInfo.biHeight/ThreadNUM)*(id + 1));
+
 	for(int i = start_i; i < end_i; i++) {
 		process_row(i, BMPData, bmpInfo, BMPTemp);
 	}
@@ -240,10 +252,10 @@ void process_column(RGBTRIPLE **BMPData, BMPINFO bmpInfo, RGBTRIPLE **BMPTemp, i
 void process_row(int i, RGBTRIPLE **BMPData, BMPINFO bmpInfo, RGBTRIPLE **BMPTemp)
 {
 	for(int j = 0; j < bmpInfo.biWidth; j++) {
-		int Top = i > 0 ? i-1 : bmpInfo.biHeight-1;
-		int Down = i<bmpInfo.biHeight-1 ? i+1 : 0;
-		int Left = j>0 ? j-1 : bmpInfo.biWidth-1;
-		int Right = j<bmpInfo.biWidth-1 ? j+1 : 0;
+		int Top = i > 0 ? i - 1 : bmpInfo.biHeight - 1;
+		int Down = i < bmpInfo.biHeight - 1 ? i + 1 :0;
+		int Left = j > 0 ? j - 1 : bmpInfo.biWidth - 1;
+		int Right = j < bmpInfo.biWidth - 1 ? j + 1 : 0;
 		BMPTemp[i][j].rgbBlue = (double)(BMPData[i][j].rgbBlue+BMPData[Top][j].rgbBlue+BMPData[Top][Left].rgbBlue+BMPData[Top][Right].rgbBlue+BMPData[Down][j].rgbBlue+BMPData[Down][Left].rgbBlue+BMPData[Down][Right].rgbBlue+BMPData[i][Left].rgbBlue+BMPData[i][Right].rgbBlue)/9+0.5;
 		BMPTemp[i][j].rgbGreen = (double)(BMPData[i][j].rgbGreen+BMPData[Top][j].rgbGreen+BMPData[Top][Left].rgbGreen+BMPData[Top][Right].rgbGreen+BMPData[Down][j].rgbGreen+BMPData[Down][Left].rgbGreen+BMPData[Down][Right].rgbGreen+BMPData[i][Left].rgbGreen+BMPData[i][Right].rgbGreen)/9+0.5;
 		BMPTemp[i][j].rgbRed = (double)(BMPData[i][j].rgbRed+BMPData[Top][j].rgbRed+BMPData[Top][Left].rgbRed+BMPData[Top][Right].rgbRed+BMPData[Down][j].rgbRed+BMPData[Down][Left].rgbRed+BMPData[Down][Right].rgbRed+BMPData[i][Left].rgbRed+BMPData[i][Right].rgbRed)/9+0.5;
